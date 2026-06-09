ef835c2b6b8322f954266e719d89a3ac6f4e2ad0
Aiman2017 <50306902+Aiman2017@users.noreply.github.com>
2026-06-09 09:40:57 +0300

    Simplify pagination by using built-in links
    Removed custom pagination markup and replaced it with built-in pagination links.

--- resources/views/front/blog.blade.php
+++ resources/views/front/blog.blade.php
@@ -86,52 +86,9 @@
 
         {{-- Pagination --}}
   
- @if ($paginator->hasPages())
-    <nav>
-        <ul class="pagination justify-content-center">
-
-            {{-- Previous Page Link --}}
-            @if ($paginator->onFirstPage())
-                <li class="page-item disabled">
-                    <span class="page-link">&lsaquo;</span>
-                </li>
-            @else
-                <li class="page-item">
-                    <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev">&lsaquo;</a>
-                </li>
-            @endif
-
-            {{-- Pagination Elements --}}
-            @foreach ($elements as $element)
-                @if (is_string($element))
-                    <li class="page-item disabled">
-                        <span class="page-link">{{ $element }}</span>
-                    </li>
-                @endif
-
-                @if (is_array($element))
-                    @foreach ($element as $page => $url)
-                        <li class="page-item {{ $page == $paginator->currentPage() ? 'active' : '' }}">
-                            <a class="page-link" href="{{ $url }}">{{ $page }}</a>
-                        </li>
-                    @endforeach
-                @endif
-            @endforeach
-
-            {{-- Next Page Link --}}
-            @if ($paginator->hasMorePages())
-                <li class="page-item">
-                    <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next">&rsaquo;</a>
-                </li>
-            @else
-                <li class="page-item disabled">
-                    <span class="page-link">&rsaquo;</span>
-                </li>
-            @endif
-
-        </ul>
-    </nav>
-@endif
+        <div styles="margin-left: 10px, margin-right:10px" class="mt-5">
+    {{ $posts->links() }}
+</div>
         @endif
     </div>
 </section>
